1399ec5316105a35befbacf3d0e8f22f32502c58
mssjew <86316072+mssjew@users.noreply.github.com>
2022-09-23 14:05:09 +0400

    .

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const AGK_GROUP = "120363026257640092@g.us"; //#1234
 
 // Chemmanur Jewellers
 
-const PRICE_CORRECTOR = 3;
+const PRICE_CORRECTOR = 5.5;
 
 const hamzaNumber = "97338999888@c.us";
 const sajeevNumber = "919946147016@c.us";
