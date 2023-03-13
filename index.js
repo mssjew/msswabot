1fdadf644338e78a6afba8a4531b3396f164398a
hmzsjd <github@hamzasajid.com>
2023-03-13 23:51:36 +0300

    .

--- index.js
+++ index.js
@@ -403,7 +403,7 @@ const quantityCalc = (arr) => {
 //     });
 
 //   })
-// });
+// });;
 
 // schedule.scheduleJob(date1, () => {
 //   alertsGroup1.forEach((group) => {
@@ -905,7 +905,7 @@ client.on("message", async (message) => {
         if (message.from === ALSEEF_GROUP || message.from === MASHALLAH_GROUP || message.from === NEW_MASHALLAH_GROUP) {
           const ttRate =
           (price + PRICE_CORRECTOR) * 1.417;
-        const ttPrice = Math.floor(ttRate) - 3;
+        const ttPrice = Math.floor(ttRate) - 1.5;
         message.reply(
           `Current TT Rate: *BD${ttPrice}*`
         );
@@ -1076,7 +1076,7 @@ client.on("message", async (message) => {
             let ttPrice;
 
             if (message.from === ALSEEF_GROUP || message.from === MASHALLAH_GROUP || message.from === NEW_MASHALLAH_GROUP) {
-              ttPrice = Math.floor(ttRate) - 3;
+              ttPrice = Math.floor(ttRate) - 1.5;
             } else {
                ttPrice = Math.round(ttRate) + TT_PREMIUM;
             }
