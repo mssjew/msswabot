ee91250eb8360e1c5eea3a444989b2f5f3908cc6
mssjew <86316072+mssjew@users.noreply.github.com>
2023-08-05 08:53:22 +0300

    .

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ const { MessageMedia } = require("whatsapp-web.js");
 
 const { Client, LocalAuth } = require("whatsapp-web.js");
 
-var TT_PREMIUM = 0;
+var TT_PREMIUM = -1;
 const VALID_CODES = [
   "#6572",
   "#6950",
@@ -899,7 +899,7 @@ client.on("message", async (message) => {
           message.from === JUMBO_GROUP
         ) {
           const ttRate = (price + PRICE_CORRECTOR) * 1.417;
-          const ttPrice = Math.floor(ttRate) -2;
+          const ttPrice = Math.floor(ttRate) -3;
           message.reply(`Current TT Rate: *BD${ttPrice}*`);
         } else {
           const ttRate = (price + PRICE_CORRECTOR) * 1.417;
@@ -1057,7 +1057,7 @@ client.on("message", async (message) => {
               message.from === NEW_MASHALLAH_GROUP ||
               message.from === JUMBO_GROUP
             ) {
-              ttPrice = Math.floor(ttRate) -2;
+              ttPrice = Math.floor(ttRate) -3;
             } else {
               ttPrice = Math.floor(ttRate) + TT_PREMIUM;
             }
