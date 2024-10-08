71a1c5b05eba00709f3804b554548af4a9e00196
hmzsjd <github@hamzasajid.com>
2024-10-08 12:18:14 +0300

    .

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const schedule = require("node-schedule");
 var emoji = require("node-emoji");
 
-const API_KEY = "K4Dlr-8Az2ZN9sxp72y1EC";
+const API_KEY = "K4Dlr-8Az2ZN9sxp72y1";
 
 console.log(`Running Node.js version ${process.version}`);
 
